5eecd444ab0b451ae18e85412003c3b1cd20eeb0
Vincent-vst <louise.insomnia@gmail.com>
2022-01-06 22:41:14 +0100

    twig fun

--- metropolis/src/Controller/homeFilm.php
+++ metropolis/src/Controller/homeFilm.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Film;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 /* use Symfony\Component\BrowserKit\Response; */
 
-class homeFilm 
+class homeFilm extends AbstractController
 {
 
     /**
@@ -21,7 +22,10 @@ class homeFilm
         $film->setNom('pulp fiction'); 
         $film->setDescription('film cool');
         $film->setNote(3);
-        return new Response($film->getNom());
+        /* return new Response($film->getNom()); */
+        return $this->render('base.html.twig', [
+            "film" => $film
+        ]);
     }
 
 }
