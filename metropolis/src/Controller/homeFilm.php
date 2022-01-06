9340546b7a026acefc1149c2b3945192817fcc18
Vincent-vst <louise.insomnia@gmail.com>
2022-01-06 22:25:40 +0100

    routing learning

--- metropolis/src/Controller/homeFilm.php
+++ metropolis/src/Controller/homeFilm.php
@@ -1,13 +1,27 @@
 <?php
-// src/Controller/LuckyController.php
+
 namespace App\Controller;
 
-use Symfony\Component\BrowserKit\Response;
+use App\Entity\Film;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/* use Symfony\Component\BrowserKit\Response; */
 
 class homeFilm 
 {
+
+    /**
+     * @Route("/", name="home")
+     */
+
+
     public function hello(){
-        return new Response("hello world");
+        /* return new Response("hello world"); */
+        $film = new Film(); 
+        $film->setNom('pulp fiction'); 
+        $film->setDescription('film cool');
+        $film->setNote(3);
+        return new Response($film->getNom());
     }
 
 }
