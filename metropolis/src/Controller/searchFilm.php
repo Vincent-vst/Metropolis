b461f8aed360bbd0e8d8c627a62ca8abbe3f5e02
Vincent-vst <louise.insomnia@gmail.com>
2022-01-12 19:19:35 +0100

    API works

--- metropolis/src/Controller/searchFilm.php
+++ metropolis/src/Controller/searchFilm.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 class searchFilm  {
     public static function search ($nomFilm) {
         $descriptionFilm = 'lorem';
-      $nomFilm = 'PulpFiction';
-      $apiKey = '3830c4bd'; 
-       
-      $url = "http://www.omdbapi.com/?apikey=" . $apiKey . "&t=" . $nomFilm;
-       
-      $response = file_get_contents($url);
+      /* $nomFilm = 'PulpFiction'; */
+        $apiKey = '3830c4bd'; 
+         
+        $url = "http://www.omdbapi.com/?apikey=" . $apiKey . "&t=" . $nomFilm;
+         
+        $response = file_get_contents($url);
 
 
-      $plot = json_decode($response,true)["Plot"];
+        $descriptionFilm = json_decode($response,true)["Plot"];
 
 
         return $descriptionFilm;
