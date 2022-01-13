273a3b416015ecc26992d7f04ae4631620beb527
Vincent-vst <louise.insomnia@gmail.com>
2022-01-13 09:00:09 +0100

    delete film

--- metropolis/src/Controller/descriptionFilm.php
+++ metropolis/src/Controller/descriptionFilm.php
@@ -9,38 +9,39 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 
 
    class descriptionFilm extends AbstractController{
      /**
      * @Route("/details/{id}", name="description")
+     * @Route("/", name="home")
      */
 
 
-    public function show( ManagerRegistry $doctrine, $id, Request $request): Response {
+    public function show( ManagerRegistry $doctrine, $id, Request $request, EntityManagerInterface $em): Response {
         $film = $doctrine->getRepository(Film::class)->find($id);
 
         $form = $this->createFormBuilder()
-             ->add('submitForm', SubmitType::class, ['label'=>'delete film'])
+             ->add('submitForm', SubmitType::class, ['label'=>'delete film '])
              ->getForm()
         ; 
 
-        if($form->isSubmitted() && $form->isValid()){
 
+        $form ->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->remove($film); 
+            $em ->flush(); 
             return $this->redirectToRoute('home');
         }
 
-        $form ->handleRequest($request);
 
    
 
         return $this->render('details/description.html.twig', ['film' => $film, 'deleteFilm' => $form->createView()]);
-        /* return $this->render('form/form.html.twig', [ */
-        /*     'formFilm' => $form->createView() */
-        /* ]); */
+
 
     }
 
