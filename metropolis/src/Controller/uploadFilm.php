a33db1cadbce037cf06d01352faa5625f44f0ef3
Vincent-vst <louise.insomnia@gmail.com>
2022-01-13 09:48:30 +0100

    added twig upload

--- metropolis/src/Controller/uploadFilm.php
+++ metropolis/src/Controller/uploadFilm.php
@@ -6,6 +6,8 @@ namespace App\Controller;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Film;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,10 +18,16 @@ class uploadFilm extends AbstractController
      * @Route("/upload", name="upload")
      */
 
-    public function upload(ManagerRegistry $doctrine) 
+    public function upload(ManagerRegistry $doctrine, FormBuilderInterface $builder) 
     {
         $text = "hello"; 
-
+        $builder 
+            ->add('file', FileType::class, [
+                'mapped' => false,
+                'label' => "uplad CSV file"
+            ])
+        ;
+        
 
     }
 
