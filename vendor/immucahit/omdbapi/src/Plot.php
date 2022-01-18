c123e01d4f61790e57b6c18b3500d021781ec4a3
Vincent-vst <louise.insomnia@gmail.com>
2022-01-18 15:13:36 +0100

    deleting some files

--- vendor/immucahit/omdbapi/src/Plot.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace OMDBAPI;
-
-class Plot{
-    const SHORT = 'short';
-    const FULL = 'full';
-}
--- vendor/immucahit/omdbapi/src/Type.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OMDBAPI;
-
-class Type{
-    const MOVIE = 'movie';
-    const SERIES = 'series';
-    const EPISODE = 'episode';
-}
